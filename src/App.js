4045c236a9b37ccd39d7f8bc92ddfdb6d9ee5d73
Sam Usachev <semenusachev10@gmail.com>
2022-11-16 16:32:29 +0300

    file format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import './App.css';
 
 function App(props) {
   return (
